a506a6097566234fc8f3e398982b88ee3cdcf6d3
Camille Pressensé <camillefasani@gmail.com>
2024-11-13 11:05:22 +0100

    count words

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 def main() :
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    count_words = countwords(text)
+    print(count_words)
+
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def countwords(fulltext):
+    words = fulltext.split()
+    return len(words)
+    
+
 main ()
 
 
